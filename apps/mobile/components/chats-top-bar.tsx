a2bdad397ff0851d9ccef022a7ae338e22cbe6fa
Pravenn18 <insanepraveen2018@gmai.com>
2024-11-10 01:39:55 +0530

    added atom

--- apps/mobile/components/chats-top-bar.tsx
+++ apps/mobile/components/chats-top-bar.tsx
@@ -1,10 +1,14 @@
+import { nameAtom } from '@/atom/userAtom';
+import { useAtom } from 'jotai';
 import { View, Text } from 'react-native';
 
 const ChatsTopBar = () => {
-  return (
+  const [name] = useAtom(nameAtom);
+return (
     <View className='flex w-full'>
         <View className='flex-row justify-between '>
             <Text className='text-3xl p-4 font-medium text-white'>Whatsapp</Text>
+            <Text className='text-lg p-4 font-normal text-white'>Hello, {name}</Text>
         </View>
     </View>
   );
