2c52cbd688108246fb61096cf156dfd83856a4d5
Pravenn18 <insanepraveen2018@gmai.com>
2024-11-10 13:40:07 +0530

    updated messagesService.ts

--- apps/mobile/utils/messageService.ts
+++ apps/mobile/utils/messageService.ts
@@ -1,22 +1,13 @@
-// utils/messageService.ts
-import { supabase } from './supabase';
+import { supabase } from "./supabase";
 
-export const sendMessage = async (senderId: string, receiverId: string, messageContent: string) => {
-    const { data, error } = await supabase
-        .from('messages')
-        .insert([
-            {
-                sender_id: senderId,
-                reciever_id: receiverId,
-                status: 'sent',
-                message: messageContent,
-            }
-        ]);
+export const sendMessage = async (sender: string, receiver: string, content: string) => {
+  const { data, error } = await supabase
+    .from('messages')
+    .insert([{ sender_id: sender, reciever_id: receiver, message: content, status: "Sent" }]);
 
-    if (error) {
-        console.error('Error sending message:', error);
-        return null;
-    }
-
-    return data;
+  if (error) {
+    console.error('Error sending message:', error);
+  }
+  
+  return data;
 };
